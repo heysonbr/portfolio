bc042bbe6020df16b6e3815275afb31169e4870c
Heyson <heysonbr@outlook.es>
2024-06-06 23:27:05 +0200

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+export default function Navbar() {
+      return (
+        <nav className="container w-64 mx-auto flex justify-center p-2 my-5 bg-red-600 rounded-full">
+          
+          <div className="links ">
+            <a href="/" className="px-2">Proyectos</a>
+            <a href="/create" className="px-2">Sobre mí</a>
+          </div>
+        </nav>
+      );
+    }
