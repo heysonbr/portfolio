948977fb7faab170c5bfc3165e150a8f98c0cddd
Heyson <heysonbr@outlook.es>
2024-06-09 01:14:54 +0200

    Actualizar estilos de la barra de navegación y agregar animación de carga en la página de habilidades

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -27,14 +27,17 @@ export default function stack() {
 
   return (
     <div className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl">
-      <h2 className="flex items-center text-3xl font-semibold gap-x-3 text-black/80 dark:text-white">
+      <h2 className="flex items-center text-3xl font-semibold gap-x-3 text-black/80 dark:text-white ">
         Habilidades
       </h2>
-      <div className="mt-14 backdrop-blur-sm bg-white/10 w-auto h-auto rounded-xl flex justify-around content-cente  ">
+        <div className="flex items-center mt-20"> 
+       <span class="relative inline-flex overflow-hidden rounded-xl p-[2px] ">
+            <span class="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#d7b4fe_0%,#7f2dda_50%,#d7b4fe_100%)] "></span>
+      <div className="  backdrop-blur-sm bg-gray-800 w-auto h-auto rounded-xl flex justify-around content-cente z-5 ">
 
         
-        
-        <div className="p-5 ">
+     
+        <div className="p-5">
           
           <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
             Lenguajes
@@ -71,6 +74,8 @@ export default function stack() {
         <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">+ GIT</div>
         </div>
        
+      </div>
+      </span>
       </div>
     </div>
   );
