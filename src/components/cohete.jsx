874e61ff158e269f00ea9b0e1c3c3d8f1d64c312
Heyson <heysonbr@outlook.es>
2024-06-10 23:00:25 +0200

    Actualizar estilos y agregar sombra al componente de cohete en la página de inicio

--- src/components/cohete.jsx
+++ src/components/cohete.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 export default function Cohete() {
   return (
     <div style={{ width: '200px', height: '200px' }}>
-      <Spline scene="https://prod.spline.design/iX6xqlmYP7nuwIcE/scene.splinecode" style={{ width: '100%', height: '100%' , borderRadius:'200px' }} />
+      <Spline scene="https://prod.spline.design/iX6xqlmYP7nuwIcE/scene.splinecode" style={{ width: '100%', height: '100%', borderRadius: '200px', boxShadow: '0px 0px 117px 51px rgba(122,28,122,0.8)' }} />
     </div>
   );
 }
