3529aa1b73f661eb22a4285be0ac5ea334b64ae3
Heyson <heysonbr@outlook.es>
2024-06-13 11:29:48 +0200

    Actualizar componente SobreMi con información personal

--- src/components/sobremi.jsx
+++ src/components/sobremi.jsx
@@ -3,7 +3,7 @@ export const SobreMi = () => {
         <div className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl">
             <h2 className="text-4xl pb-20">Sobre mí</h2>
             <p>
-                ¡Hola, soy Heyson!,      
+                ¡Hola, soy Heyson!,  Nacido en Nicargua llevo desde los 6 años viviendo en españa soy un apasionado de la tecnología y la programación, me encanta aprender cosas nuevas y compartir mis conocimientos con los demás.   
             </p>
         </div>
     )
