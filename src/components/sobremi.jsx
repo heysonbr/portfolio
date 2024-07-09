48e494425c106983097aa3411d7520a5925869ee
Heyson <heysonbr@outlook.es>
2024-07-09 21:59:14 +0200

    Actualizar información personal en el componente SobreMi

--- src/components/sobremi.jsx
+++ src/components/sobremi.jsx
@@ -2,9 +2,13 @@ export const SobreMi = () => {
     return (
         <div className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl">
             <h2 className="text-4xl pb-20">Sobre mí</h2>
-            <p>
+            <p style={{ 
+                background: 'linear-gradient(to right, purple, blue)',
+                WebkitBackgroundClip: 'text',
+                WebkitTextFillColor: 'transparent'
+            }}>
                 ¡Hola, soy Heyson!,  Nacido en Nicargua llevo desde los 6 años viviendo en españa soy un apasionado de la tecnología y la programación, me encanta aprender cosas nuevas y compartir mis conocimientos con los demás.  Soy un desarrollador web full stack, con experiencia en tecnologías como React, Node.js, Express, MongoDB, TypeScript, entre otras. 
             </p>
         </div>
     )
-}
+} 
