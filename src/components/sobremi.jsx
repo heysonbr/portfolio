425a2b160290a166c6e96799eea86a6db1c4c83a
Heyson <heysonbr@outlook.es>
2024-07-16 15:04:48 +0200

    Actualizar estilos y corregir rotación de imagen en el componente SobreMi

--- src/components/sobremi.jsx
+++ src/components/sobremi.jsx
@@ -6,8 +6,8 @@ export const SobreMi = () => {
                 <p className="pr-0 md:pr-5">
                     Me llamo Heyson Betancourt. Empecé en la programación desde el 2016, por cosas de la vida me alejé de la programación y ahora he vuelto con más fuerza que nunca. Me encanta aprender cosas nuevas y compartir mis conocimientos con los demás. Soy un apasionado de la tecnología y me encanta aprender cosas nuevas.
                 </p>
-                <div className="rounded m-5 md:ml-0 border-double border-4 border-white">
-                    <img className="rounded origin-left rotate-12 size-80 duration-200 hover:rotate-0 md:size-auto" src="/avataAnime.avif" alt="heyson" />
+                <div className="rounded m-5 md:ml-0 border border-4 border-white">
+                    <img className="origin-left rotate-12 size-80 duration-200 hover:rotate-0 md:size-auto" src="/avataAnime.avif" alt="heyson" />
                 </div>
             </div>
         </div>
