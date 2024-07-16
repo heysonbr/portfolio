242a6dabe2511061ddbfea5488034a26b00e7a93
Heyson <heysonbr@outlook.es>
2024-07-16 20:42:35 +0200

    Actualizar estilos y corregir rotación de imagen en el componente SobreMi

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: { animation: {
+			'gradient-x': 'gradient-x 10s ease-in-out infinite',
+		  },
+		  keyframes: {
+			'gradient-x': {
+			  '0%': { 'background-size': '300% 100%', 'background-position': '200% center' },
+			  '50%': { 'background-size': '300% 100%', 'background-position': '0% center' },
+			  '100%': { 'background-size': '300% 100%', 'background-position': '-100% center' },
+			},
+		  },},
 	},
 	plugins: [],
 }
