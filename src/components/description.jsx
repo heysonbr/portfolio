9d24b5c46957bfa41c150744d300ff7f53f9ac56
Heyson <heysonbr@outlook.es>
2024-09-11 18:23:17 +0200

    Cambios navbar y estilos

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -3,15 +3,9 @@ import Gmail from "./icons/email";
 import Cohete from "./cohete";
 import { useState } from "react";
 
-
 export default function descipcion(descripcion) {
-
-
-
-
-
   return (
-    <div className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl" id="sobre-mi">
+    <div className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl" id="inicio">
       <div className="flex flex-col-reverse items-center md:flex-row justify-between">
         <div className="flex flex-col-reverse m-5 md:flex-row-reverse items-center">
           {/* <img
@@ -19,7 +13,10 @@ export default function descipcion(descripcion) {
             alt="heyson"
             className="rounded-full w-20 h-20 pr-2 mb-2 mt-3 md:mb-0 md:m-3 md:pr-2 md:order-2"
           /> */}
-          <a className="flex items-center transition md:justify-center md:hover:scale-105 md:order-1" href="https://www.linkedin.com/in/heyson-br/">
+          <a
+            className="flex items-center transition md:justify-center md:hover:scale-105 md:order-1"
+            href="https://www.linkedin.com/in/heyson-br/"
+          >
             <div className="flex items-center ">
               <span className="relative inline-flex overflow-hidden rounded-full p-[1px]">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#d7b4fe_0%,#7f2dda_50%,#d7b4fe_100%)]"></span>
@@ -35,18 +32,29 @@ export default function descipcion(descripcion) {
 
       <h1 className="text-4xl font-bold pt-5 ">{descripcion.hola} </h1>
 
-      <p className="text-lg  mt-5 ">+1 Año experiencia. <strong className="text-purple-300">Full Stack Developer</strong> de Nicaragua, residiendo en Mallorca 🇪🇸.
-        Especializado en el desarrollo de aplicaciones web únicas.</p>
-
-
-
+      <p className="text-lg  mt-5 ">
+        +1 Año experiencia.{" "}
+        <strong className="text-purple-300">Full Stack Developer</strong> de
+        Nicaragua, residiendo en Mallorca 🇪🇸. Especializado en el desarrollo de
+        aplicaciones web únicas.
+      </p>
 
       <div className="pt-7 flex ">
-        <a className="flex  rounded-full w-auto bg-gray-800 py-1 px-2 border-grey border border-solid my-1 hover:border-transparent" href="mailto:heysonbr@gmail.com"> <Gmail className="pt-1 mr-1" /> Contáctame </a>
-        <a className="flex rounded-full w-auto bg-gray-800 py-1 px-3 border-grey border border-solid m-1 hover:border-transparent" href="https://www.linkedin.com/in/heyson-br/"> <LinkedIn className="mt-1 mr-1" /> Linkedin </a>
+        <a
+          className="flex  rounded-full w-auto bg-gray-800 py-1 px-2 border-grey border border-solid my-1 hover:border-transparent"
+          href="mailto:heysonbr@gmail.com"
+        >
+          {" "}
+          <Gmail className="pt-1 mr-1" /> Contáctame{" "}
+        </a>
+        <a
+          className="flex rounded-full w-auto bg-gray-800 py-1 px-3 border-grey border border-solid m-1 hover:border-transparent"
+          href="https://www.linkedin.com/in/heyson-br/"
+        >
+          {" "}
+          <LinkedIn className="mt-1 mr-1" /> Linkedin{" "}
+        </a>
       </div>
-
-
     </div>
   );
 }
--- src/components/sobremi.jsx
+++ src/components/sobremi.jsx
@@ -1,15 +1,26 @@
 export const SobreMi = () => {
-    return (
-        <div className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl">
-            <h2 className="text-4xl pb-20">Sobre mí</h2>
-            <div className="flex flex-col md:flex-row items-center">
-                <p className="text-xl pr-0 md:pr-5   ">
-                    Me llamo Heyson Betancourt. Empecé en la programación desde el 2016, por cosas de la vida me alejé de la programación y ahora he vuelto con más fuerza que nunca. Me encanta aprender cosas nuevas y compartir mis conocimientos con los demás. Soy un apasionado de la tecnología y me encanta aprender cosas nuevas.
-                </p>
-                <div className="rounded-lg m-5 md:ml-0  border-4 border-white duration-200 hover:scale-110 ">
-                    <img className="origin-left rotate-6 size-80 duration-200 hover:rotate-0  md:size-auto" src="/avataAnime.avif" alt="heyson" />
-                </div>
-            </div>
+  return (
+    <div
+      className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl"
+      id="sobre-mi"
+    >
+      <h2 className="text-4xl pb-10">Sobre mí</h2>
+      <div className="flex flex-col md:flex-row items-center">
+        <p className="text-xl pr-0 md:pr-5   ">
+          Me llamo Heyson Betancourt. Empecé en la programación desde el 2016,
+          por cosas de la vida me alejé de la programación y ahora he vuelto con
+          más fuerza que nunca. Me encanta aprender cosas nuevas y compartir mis
+          conocimientos con los demás. Soy un apasionado de la tecnología y me
+          encanta aprender cosas nuevas.
+        </p>
+        <div className="rounded-lg m-5 md:ml-0  border-4 border-white duration-200 hover:scale-110 ">
+          <img
+            className="origin-left rotate-6 size-80 duration-200 hover:rotate-0  md:size-auto"
+            src="/avataAnime.avif"
+            alt="heyson"
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
