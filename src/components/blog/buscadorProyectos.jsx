6d9bdccade36db4c2daf43f4c18d5a96d151dd33
Heyson <heysonbr@outlook.es>
2024-09-12 12:27:27 +0200

    Actualizar datos de proyectos en el componente BuscadorProyectos.jsx

--- src/components/blog/buscadorProyectos.jsx
+++ src/components/blog/buscadorProyectos.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import projectsData from "./projects.json";
+
 import React from "../icons/react";
 import Bootstrap from "../icons/bootstrap";
 import MySQL from "../icons/mysql";
@@ -8,39 +10,31 @@ import Python from "../icons/python";
 export default function Buscador() {
   const [search, setSearch] = useState("");
 
-  const projects = [
-    {
-      title: "BeatBooK",
-      description:
-        "BeatBooK es una aplicación web que permite a los usuarios buscar eventos musicales en su ciudad, ver detalles de los eventos y comprar boletos. Los usuarios pueden registrarse, iniciar sesión, inscribirse a la eventos y comentarlos.",
-      // link: "https://svgl.vercel.app/",
-      github: "",
-      image: "/beatbook.png",
-      tags: [
-        { name: "React", class: "bg-blue-700 text-white", icon: React },
-        {
-          name: "Bootstrap",
-          class: "bg-purple-300 text-white",
-          icon: Bootstrap,
-        },
-        { name: "MySQL", class: "bg-yellow-300 text-white", icon: MySQL },
-      ],
-    },
-    {
-      title: "FFMPEG",
-      description:
-        "FFMPEG es una aplicación web que permite a los usuarios buscar eventos musicales en su ciudad, ver detalles de los eventos y comprar boletos. Los usuarios pueden registrarse, iniciar sesión, inscribirse a la eventos y comentarlos.",
-      // link: "https://svgl.vercel.app/",
-      github: "",
-      image: "/beatbook.png",
-      tags: [
-        { name: "MySQL", class: "bg-yellow-300 text-white", icon: MySQL },
-        { name: "Python", class: "bg-green-500 text-white", icon: Python },
-      ],
-    },
-
-    // ...otros proyectos
-  ];
+  const projects = projectsData.map((project) => {
+    // Aquí puedes transformar los datos del proyecto si es necesario
+    // Por ejemplo, puedes reemplazar los nombres de los iconos por los componentes de los iconos
+    const tags = project.tags.map((tag) => {
+      let icon;
+      switch (tag.icon) {
+        case "React":
+          icon = React;
+          break;
+        case "Bootstrap":
+          icon = Bootstrap;
+          break;
+        case "MySQL":
+          icon = MySQL;
+          break;
+        case "Python":
+          icon = Python;
+          break;
+        default:
+          icon = null;
+      }
+      return { ...tag, icon };
+    });
+    return { ...project, tags };
+  });
 
   const technologies = ["React", "Bootstrap", "MySQL", "Python"];
 
@@ -81,11 +75,11 @@ export default function Buscador() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {filteredProjects.map((project) => (
           <div
-            className="h-64 w-72 lg:w-64 rounded-lg py-2 text-start"
+            className="h-64 w-72 lg:w-64 rounded-lg py-2 text-start transition duration-500 hover:scale-110"
             key={project.title}
           >
             <img
-              className="rounded-lg w-full  transition duration-500 hover:scale-110 "
+              className="rounded-lg w-full   "
               src={project.image}
               alt={project.title}
             />
@@ -102,6 +96,16 @@ export default function Buscador() {
                 </li>
               ))}
             </ul>
+            <footer>
+              {project.github && (
+                <a
+                  className="border border-gray-500 rounded-lg p-1 bg-slate-900 hover:border-transparent"
+                  href={project.github}
+                >
+                  Codigo
+                </a>
+              )}
+            </footer>
           </div>
         ))}
       </div>
