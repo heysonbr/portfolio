3c2dc1db885bb27484f67b84aa258ab3f24069e2
Heyson <heysonbr@outlook.es>
2024-09-15 21:04:32 +0200

    Actualizar estilos y corregir etiquetas en el componente BuscadorProyectos.jsx

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -1,6 +1,5 @@
 import Contributions from "./Contributions";
 
-
 export default function stack() {
   const habilities = {
     lenguajes: ["JavaScript", "Python", "CSS", "HTML"],
@@ -13,14 +12,26 @@ export default function stack() {
       "Tailwind CSS",
       "Sass",
       "SQLAlchemy",
-      "Next.js", 
-      "Expo", 
-      "Firebase", 
-      "Express", 
+      "Next.js",
+      "Expo",
+      "Firebase",
+      "Express",
     ],
     controlDeVersiones: ["GIT"],
   };
 
+  const renderHabilities = (title, habilities) => (
+    <div className="p-3">
+      <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
+        {title}
+      </h4>
+      {habilities.map((hability) => (
+        <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
+          + {hability}
+        </div>
+      ))}
+    </div>
+  );
 
   return (
     <div className="container mx-auto p-5 mt-20 lg:max-w-4xl md:max-w-2xl">
@@ -31,80 +42,16 @@ export default function stack() {
         <span className="relative inline-flex overflow-hidden rounded-xl p-[2px] ">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#d7b4fe_0%,#7f2dda_50%,#d7b4fe_100%)] "></span>
           <div className="  backdrop-blur-sm bg-gray-800 w-auto h-auto rounded-xl flex justify-around content-cente z-5 ">
-            <div className="p-5">
-              <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
-                Lenguajes
-              </h4>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + JavaScript
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Python
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + CSS
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + HTML
-              </div>
-
-              <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
-                Bases de Datos
-              </h4>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + MySQL
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + SQLAlchemy
-              </div>
-            </div>
-            <div className="p-5">
-              <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
-                Frameworks y Bibliotecas
-              </h4>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + React
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + React Native
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Astro
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Flask
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Bootstrap
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Tailwind CSS
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Sass
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + SQLAlchemy
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Next.js
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Expo
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Firebase
-              </div>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + Express
-              </div>
-              <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
-                Control de Versiones
-              </h4>
-              <div className="m-2 inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-                + GIT
-              </div>
-            </div>
+            {renderHabilities("Lenguajes", habilities.lenguajes)}
+            {renderHabilities("Bases de Datos", habilities.basesDeDatos)}
+            {renderHabilities(
+              "Frameworks y Bibliotecas",
+              habilities.frameworks
+            )}
+            {renderHabilities(
+              "Control de Versiones",
+              habilities.controlDeVersiones
+            )}
           </div>
         </span>
       </div>
