03463039ab4e6b03b1f9d7a0deff32e245423a43
Heyson <heysonbr@outlook.es>
2024-09-24 20:29:21 +0200

    Actualizar lista de tecnologías en el componente BuscadorProyectos.jsx y eliminar proyecto duplicado en projects.json

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -20,15 +20,19 @@ export default function stack() {
     controlDeVersiones: ["GIT"],
   };
 
-  const renderHabilities = (title, habilities) => (
+  const renderHabilities = (habilities) => (
     <div className="p-4 text-center">
-      <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
-        {title}
-      </h4>
-      {habilities.map((hability) => (
-        <div className="m-2 inline-flex items-center justify-center gap-2 px-2 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
-          {hability}
-        </div>
+      {Object.keys(habilities).map((title) => (
+        <>
+          <h4 className="text-xl font-bold text-gray-700 dark:text-gray-100">
+            {title}
+          </h4>
+          {habilities[title].map((hability) => (
+            <div className="m-2 inline-flex items-center justify-center gap-2 px-2 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black">
+              {hability}
+            </div>
+          ))}
+        </>
       ))}
     </div>
   );
@@ -42,16 +46,7 @@ export default function stack() {
         <span className="relative inline-flex overflow-hidden rounded-xl p-[2px] ">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#d7b4fe_0%,#7f2dda_50%,#d7b4fe_100%)] "></span>
           <div className="  backdrop-blur-sm bg-gray-800 w-auto h-auto rounded-xl flex justify-around content-cente z-5 ">
-            {renderHabilities("Lenguajes", habilities.lenguajes)}
-            {renderHabilities("Bases de Datos", habilities.basesDeDatos)}
-            {renderHabilities(
-              "Frameworks y Bibliotecas",
-              habilities.frameworks
-            )}
-            {renderHabilities(
-              "Control de Versiones",
-              habilities.controlDeVersiones
-            )}
+            {renderHabilities(habilities)}
           </div>
         </span>
       </div>
