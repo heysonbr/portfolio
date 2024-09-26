4efcce7cc0f44cfb53f599682ed4eb4557262249
Heyson <heysonbr@outlook.es>
2024-09-26 21:53:04 +0200

    Actualizar información en el componente SobreMi.jsx

--- src/components/sobremi.jsx
+++ src/components/sobremi.jsx
@@ -8,15 +8,18 @@ export const SobreMi = () => {
       <div className="flex flex-col md:flex-row items-center">
         <p className="text-xl pr-0 md:pr-5   ">
           Mi nombre es{" "}
-          <span className="text-purple-300 font-semibold">Heyson Betancourt</span>. Comencé mi
-          carrera en programación en 2016, pero tuve que apartarme para trabajar
-          en el sector de la construcción durante 8 años. Ahora he vuelto con
-          más fuerza, aprovechando mis experiencias previas para aportar una
-          nueva perspectiva al desarrollo de software. Me especializo en
-          desarrollo full stack, y disfruto aprender constantemente y compartir
-          conocimientos. Mi pasión por la tecnología sigue creciendo,
-          impulsándome a enfrentar nuevos desafíos y crear soluciones
-          innovadoras.
+          <span className="text-purple-300 font-semibold">
+            Heyson Betancourt
+          </span>
+          . Comencé mi carrera en programación en 2016, pero tuve que apartarme
+          para trabajar en el sector de la construcción durante 8 años. Ahora he
+          vuelto con más fuerza, aprovechando mis experiencias previas para
+          aportar una nueva perspectiva al desarrollo de software. Me
+          especializo en desarrollo{" "}
+          <span className="text-purple-300 font-semibold">full stack</span>, y
+          disfruto aprender constantemente y compartir conocimientos. Mi pasión
+          por la tecnología sigue creciendo, impulsándome a enfrentar nuevos
+          desafíos y crear soluciones innovadoras.
         </p>
         {/* <div className="rounded-lg m-5 md:ml-0  border-4 border-white duration-200 hover:scale-110 ">
           <img
